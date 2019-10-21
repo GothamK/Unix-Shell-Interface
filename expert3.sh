8e74fddc809208b11cc7130808513056d60256d6
GothamK <43772245+GothamK@users.noreply.github.com>
2019-10-21 21:35:50 +0530

    Improved DIsk System status command
    Included border cases

--- expert3.sh
+++ expert3.sh
@@ -3,16 +3,16 @@ echo "           SYSTEM STATUS COMMANDS"
 echo -e "\n"
 echo -e "\n"
 
-echo "   1 -- Display current date and time"
-echo "   2 -- Current disk usage"
-echo "   3 -- List current local and environmental"
-echo "   4 -- Display process status information  "
-echo "   5 -- Exit program"
+echo "   1 -- Display Date and time"
+echo "   2 -- Current Disk usage"
+echo "   3 -- List Current local and environmental"
+echo "   4 -- Display Process status information"
+echo "   5 -- Exit Program"
 echo -e "\n"
 breaker=1
 while [ "$breaker" -eq 1 ]
 do
-echo -n "        Enter your choice:"
+echo -n "        Enter your choice: "
 read input
 
 if [[ $input -eq 5 ]] ; then
@@ -43,4 +43,11 @@ if [[ $input -eq 3 ]] ; then
 printenv > t.txt
 more t.txt
 fi
+
+if[[ $input -eq 0 ]] ; then
+printenv > t.txt
+more t.txt
+fi
 done
+
+
